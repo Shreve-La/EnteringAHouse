58afa2345e5f4466b3cbbeaff4b3ae0c40f39b79
Shreve-La <shrevela@gmail.com>
2017-06-12 16:25:57 -0400

    Initial Commit

---
+++ EnteringAHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnteringAHouse
+//
+//  Created by swcl on 2017-06-12.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by swcl on 2017-06-12.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
